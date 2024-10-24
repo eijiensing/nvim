f640a2e9de29ce6fbf30dc15f869388414397470
Eiji Ensing <eijitron@gmail.com>
2024-10-24 12:05:10 +0200

    tabs and tailwind

--- init.lua
+++ init.lua
@@ -62,8 +62,8 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.list = false
+vim.opt.listchars = { tab = '>> ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -484,6 +484,7 @@ require('lazy').setup({
           },
         },
         rust_analyzer = {},
+        tailwindcss = {},
         eslint = {
           settings = {
             packageManager = 'pnpm', -- Or yarn, pnpm, depending on what you use
